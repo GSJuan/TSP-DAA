abe313a0df304c178d82c0ffec5c2740670ab311
JuanGarciaSantos <alu0101325583@ull.edu.es>
2022-04-03 21:00:16 +0100

    Instance Generator Done

--- TSP/InstanceGenerator.cs
+++ TSP/InstanceGenerator.cs
@@ -8,19 +8,51 @@ namespace TSP
     {
         public List<List<string>> graph;
 
-        public InstanceGenerator (int nodes)
+        public List<char> letters = new List<char>() { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        public InstanceGenerator ()
         {
-            Random rnd = new Random();
-            
-
-
+                     
         }
-        public void WriteFile(string filename)
+        
+        public void Generate (int nodes)
         {
-            System.IO.StreamWriter file = new System.IO.StreamWriter(filename);
-            file.WriteLine("NAME : " + filename);
-           
+            Console.WriteLine("Generating instance of size {0}...", nodes);
+            Random rnd = new Random();
+            int numberOfTransitions = (nodes * (nodes - 1)) / 2;
+            string fileName = nodes.ToString() + "_nodes.txt";
+            string filePath = @"C:\Users\Juan\source\repos\TSP-DAA\TSP\problems\";
+            System.IO.StreamWriter file = new System.IO.StreamWriter(filePath + fileName);
+            List<string> nodeNames = new List<string>();
+            int offset = (nodes / letters.Count) + 1;
+
+            for (int i = 1; i <= offset; i++)
+            {
+                int limit = nodes;
+                if (i == offset)
+                {
+                    limit = nodes - (letters.Count * (i - 1));
+                }
+
+                for (int j = 0; j < limit; j++)
+                {
+                    nodeNames.Add(new String(letters[j], i));
+                }
+            }
+            file.WriteLine(nodes.ToString());
+            for (int i = 0; i < nodeNames.Count; i++)
+            {
+                string node1 = nodeNames[i];
+                for (int j = i + 1; j < nodeNames.Count; j++)
+                {
+                    string node2 = nodeNames[j];
+                    int cost = rnd.Next(1, 100);
+                    file.WriteLine(node1 + " " + node2 + " " + cost.ToString());
+                }
+            }
             file.Close();
-        } 
+            
+            Console.WriteLine("Instance written to file {0}", fileName);
+        }
     }
 }
--- TSP/Program.cs
+++ TSP/Program.cs
@@ -9,45 +9,68 @@ namespace TSP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi!, please enter the complete path of the folder containing the input problems");
-            string folderPath = @""; // C:\Users\Juan\source\repos\TSP-DAA\TSP\problems
-            folderPath += Console.ReadLine();
 
-            List<string> filesPaths = new List<string>();
-            foreach (string filePath in Directory.GetFiles(folderPath, "*.txt"))
+            Console.Write("Hi, choose 0 if you wish to solve all existing tsp problems in problems folder, or 1 if you wish to generate a new instance:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 0)
             {
+
+                Console.WriteLine("Hi!, please enter the complete path of the folder containing the input problems");
+                string folderPath = @""; // C:\Users\Juan\source\repos\TSP-DAA\TSP\problems
+                folderPath += Console.ReadLine();
+
+                List<string> filesPaths = new List<string>();
+                foreach (string filePath in Directory.GetFiles(folderPath, "*.txt"))
+                {
                     filesPaths.Add(filePath);
-            }          
+                }
 
-            TableDrawing table = new TableDrawing(200);
-            Stopwatch chrono = new Stopwatch();
-            
-            table.PrintLine();
-            table.PrintRow("Instance", "BruteForce Value", "BruteForce Time", "Greedy Value", "Greedy Time", "Dynamic Programming Value", "Dynamic Programming Time");
-            table.PrintLine();
+                TableDrawing table = new TableDrawing(200);
+                Stopwatch chrono = new Stopwatch();
 
-            foreach(string filePath in filesPaths)
-            {
-                string filename = filePath.LastIndexOf('\\') != -1 ? filePath.Substring(filePath.LastIndexOf('\\') + 1) : filePath;
-                Graph problem = new Graph(filePath);
-
-                Greedy greed = new Greedy();
-                chrono.Restart();
-                List<string> greedMinPath = greed.Solve(problem, "A");
-                chrono.Stop();
-                var elapsedMsGreedy = chrono.Elapsed;
-                int greedCost = problem.GetPathCost(greedMinPath);
-
-                BruteForce brute = new BruteForce();
-                chrono.Restart();
-                List<string> bruteMinPath = brute.Solve(problem, "A");
-                chrono.Stop();
-                var elapsedMsBrute = chrono.Elapsed;
-                int bruteCost = problem.GetPathCost(bruteMinPath);
-
-                table.PrintRow(filename, bruteCost.ToString(), elapsedMsBrute.ToString(), greedCost.ToString(), elapsedMsGreedy.ToString(), "0", "0");
                 table.PrintLine();
+                table.PrintRow("Instance", "BruteForce Value", "BruteForce Time", "Greedy Value", "Greedy Time", "Dynamic Programming Value", "Dynamic Programming Time");
+                table.PrintLine();
+
+                foreach (string filePath in filesPaths)
+                {
+                    string filename = filePath.LastIndexOf('\\') != -1 ? filePath.Substring(filePath.LastIndexOf('\\') + 1) : filePath;
+                    Graph problem = new Graph(filePath);
+
+                    Greedy greed = new Greedy();
+                    chrono.Restart();
+                    List<string> greedMinPath = greed.Solve(problem, "A");
+                    chrono.Stop();
+                    var elapsedMsGreedy = chrono.Elapsed;
+                    int greedCost = problem.GetPathCost(greedMinPath);
+
+                    BruteForce brute = new BruteForce();
+                    chrono.Restart();
+                    List<string> bruteMinPath = brute.Solve(problem, "A");
+                    chrono.Stop();
+                    var elapsedMsBrute = chrono.Elapsed;
+                    int bruteCost = problem.GetPathCost(bruteMinPath);
+
+                    table.PrintRow(filename, bruteCost.ToString(), elapsedMsBrute.ToString(), greedCost.ToString(), elapsedMsGreedy.ToString(), "0", "0");
+                    table.PrintLine();
+                }
             }
+            else if (choice == 1)
+            {
+                Console.Write("Write the number of nodes in the graph you wish to generate:");
+                int nodes = Convert.ToInt32(Console.ReadLine());
+                if(nodes > 0)
+                {
+                    InstanceGenerator generator = new InstanceGenerator();
+                    generator.Generate(nodes);
+                }
+                else Console.WriteLine("Wrong input, that was not a number > 0");
+
+
+            }
+
+            else Console.WriteLine("Wrong input");
+
         }
     }
 }
